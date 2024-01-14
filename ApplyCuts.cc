26e0f015cda8daf5e13a2995947cb825ea07ece9
Dinupa Nawarathne <dinupa3@gmail.com>
2024-01-13 19:57:10 -0600

    added RDataFrame

--- ApplyCuts.cc
+++ ApplyCuts.cc
@@ -18,7 +18,6 @@ TString LH2_jpsi_data = "/seaquest/users/chleung/pT_ReWeight/mc_jpsi_LH2_M027_S0
 
 int nbins = 50;
 
-
 void GetAcceptance(TString var_name, double xmin, double xmax, TString kin_cuts, TString hist_name, TString target, TString particle, TString particle_MC)
 {
     TString MC_cuts = Form("%s && %s && %s && %s && %s && %s && %s", chuckCutsPositive_2111v42_tmp.Data(), chuckCutsNegative_2111v42_tmp.Data(), chuckCutsDimuon_2111v42.Data(), physicsCuts_noMassCut_2111v42_tmp.Data(), occCuts_2111v42_Run56.Data(), particle_MC.Data(), kin_cuts.Data());
@@ -86,20 +85,20 @@ void GetAcceptance(TString var_name, double xmin, double xmax, TString kin_cuts,
     t->Draw();
     can->Update();
 
-    TLine* l_min = new TLine(mean - 0.75* fwhm, 0., mean - 0.75* fwhm, ymax);
+    TLine* l_min = new TLine(mean - 1.5* fwhm, 0., mean - 1.5* fwhm, ymax);
     l_min->SetLineColor(kRed);
     l_min->Draw();
-    TLine* l_max = new TLine(mean + 0.75* fwhm, 0., mean + 0.75* fwhm, ymax);
+    TLine* l_max = new TLine(mean + 1.5* fwhm, 0., mean + 1.5* fwhm, ymax);
     l_max->SetLineColor(kRed);
     l_max->Draw();
     can->Update();
     can->SaveAs(hist_save.Data());
 
-    TString outputs = Form("--> Fiducial cuts = %.1f < %s < %.1f", mean - 0.75* fwhm, var_name.Data(), mean + 0.75* fwhm);
+    TString outputs = Form("--> Fiducial cuts = %.1f < %s < %.1f", mean - 1.5* fwhm, var_name.Data(), mean + 1.5* fwhm);
     cout << outputs.Data() << endl;
 }
 
 void ApplyCuts()
 {
-    GetAcceptance("x1_st1", -30., 20., "0.4 < xF && xF < 0.6", "LH2_jpsi_x1_st1_xF0", "targetPos==1", jPsiCut, jPsiCut_MC);
+    GetAcceptance("x1_st1", -20., 15., "0.4 < xF && xF < 0.6", "LH2_jpsi_x1_st1_xF0", "targetPos==1", jPsiCut, jPsiCut_MC);
 }
