0bb6e01871e12b798b8f29373e5dc45e5bd6be43
Dinupa Nawarathne <dinupa3@gmail.com>
2024-01-16 15:16:56 -0600

    minor buug

--- DetectorSmearing.cc
+++ DetectorSmearing.cc
@@ -18,10 +18,6 @@ int bins = 20;
 
 void plotSmearing(ROOT::RDF::RNode dataFrame, TString variable, TString kinematics, TString smearing)
 {
-    TString hTrue = Form("hist_m%s_%s", variable.Data(), smearing.Data());
-    TString hReco = Form("hist_%s_%s", variable.Data(), smearing.Data());
-    TString tTitle = Form("; True %s; counts", variable.Data());
-    TString rTitle = Form("; Reco. %s; counts", variable.Data());
     TString variableTrue = Form("m%s", variable.Data());
     TString smearingTitle = Form("Smearing ; True %s; Reco. %s", variable.Data(), variable.Data());
 
@@ -30,16 +26,14 @@ void plotSmearing(ROOT::RDF::RNode dataFrame, TString variable, TString kinemati
     auto xmin = df_with_kinematics.Min(variable.Data());
     auto xmax = df_with_kinematics.Max(variable.Data());
 
-    auto hist_true = df_with_kinematics.Histo1D({hTrue.Data(), tTitle.Data(), bins, *xmin, *xmax}, variableTrue.Data(), "ReWeight");
-    auto hist_reco = df_with_kinematics.Histo1D({hReco.Data(), rTitle.Data(), bins, *xmin, *xmax}, variable.Data(), "ReWeight");
+    RooUnfoldResponse matrix(bins, *xmin, *xmax);
 
-    auto hist_smearing = new TH2D(smearing.Data(), smearingTitle.Data(), bins, *xmin, *xmax, bins, *xmin, *xmax);
+    auto sMatrix = df_with_kinematics.Fill(matrix, {variable.Data(), variableTrue.Data(), "ReWeight"});
 
-    RooUnfoldResponse smearing_matrix(hist_reco.GetPtr(), hist_true.GetPtr(), smearing.Data(), smearingTitle.Data());
-
-    auto R = smearing_matrix.HresponseNoOverflow();
+    auto R = sMatrix.HresponseNoOverflow();
     auto c1 = new TCanvas();
     R->SetStats(0);
+    R->SetNameTitle(smearing.Data(), smearingTitle.Data())
     R->Draw("COLZ");
     c1->Draw();
     c1->SaveAs("smearing.png");
