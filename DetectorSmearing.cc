c3fb771bccd12c6e89f3cfd5c22deca27cd7eb89
Dinupa Nawarathne <dinupa3@gmail.com>
2024-01-16 15:38:08 -0600

    roounfold bug

--- DetectorSmearing.cc
+++ DetectorSmearing.cc
@@ -41,5 +41,5 @@ void plotSmearing(ROOT::RDF::RNode dataFrame, TString varTrue, TString varReco, 
 
 void DetectorSmearing()
 {
-    plotSmearing(df_LH2_jpsi_with_basic_cuts, "Mass", "0.4 < xF < 0.6", "smearing_xF0");
+    plotSmearing(df_LH2_jpsi_with_basic_cuts, "Mass", "mass", "0.4 < xF < 0.6", "smearing_xF0");
 }
