c7fd748a65ddfba81864fae9e6df1360df48e5c9
Dinupa Nawarathne <dinupa3@gmail.com>
2024-01-16 17:55:52 -0600

    added variables with kinematic bins

--- DetectorSmearing.hh
+++ DetectorSmearing.hh
@@ -23,16 +23,20 @@ TString basic_jpsi_cuts = Form("%s && %s && %s && %s && %s && %s", chuckCutsPosi
 TString basic_psip_cuts = Form("%s && %s && %s && %s && %s && %s", chuckCutsPositive_2111v42_tmp.Data(), chuckCutsNegative_2111v42_tmp.Data(), chuckCutsDimuon_2111v42.Data(), physicsCuts_noMassCut_2111v42_tmp.Data(), occCuts_2111v42_Run56.Data(), psipCut.Data());
 
 RDataFrame df_LH2_jpsi_data("Tree", LH2_jpsi_data.Data());
-auto df_LH2_jpsi_with_basic_cuts = df_LH2_jpsi_data.Filter(basic_jpsi_cuts.Data());
+auto df_LH2_jpsi_with_basic_cuts0 = df_LH2_jpsi_data.Filter(basic_jpsi_cuts.Data());
+auto df_LH2_jpsi_with_basic_cuts = df_LH2_jpsi_with_basic_cuts0.Define("pT", "sqrt(dpx* dpx + dpy* dpy)");
 
 RDataFrame df_LD2_jpsi_data("Tree", LD2_jpsi_data.Data());
-auto df_LD2_jpsi_with_basic_cuts = df_LD2_jpsi_data.Filter(basic_jpsi_cuts.Data());
+auto df_LD2_jpsi_with_basic_cuts0 = df_LD2_jpsi_data.Filter(basic_jpsi_cuts.Data());
+auto df_LD2_jpsi_with_basic_cuts = df_LD2_jpsi_with_basic_cuts0.Define("pT", "sqrt(dpx* dpx + dpy* dpy)");
 
 RDataFrame df_LH2_psip_data("Tree", LH2_psip_data.Data());
-auto df_LH2_psip_with_basic_cuts = df_LH2_psip_data.Filter(basic_psip_cuts.Data());
+auto df_LH2_psip_with_basic_cuts0 = df_LH2_psip_data.Filter(basic_psip_cuts.Data());
+auto df_LH2_psip_with_basic_cuts = df_LH2_psip_with_basic_cuts0.Define("pT", "sqrt(dpx* dpx + dpy* dpy)");
 
 RDataFrame df_LD2_psip_data("Tree", LD2_psip_data.Data());
-auto df_LD2_psip_with_basic_cuts = df_LD2_psip_data.Filter(basic_psip_cuts.Data());
+auto df_LD2_psip_with_basic_cuts0 = df_LD2_psip_data.Filter(basic_psip_cuts.Data());
+auto df_LD2_psip_with_basic_cuts = df_LD2_psip_with_basic_cuts0.Define("pT", "sqrt(dpx* dpx + dpy* dpy)");
 
 
 TString varRecos[3] = {"mass", "pT", "xF"};
