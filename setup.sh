4d0e191d3a027842397c7c8d128efa7b4f76f2f2
Dinupa Nawarathne <dinupa3@gmail.com>
2024-01-16 14:39:20 -0600

    added files to calculate the detector smearing

--- setup.sh
+++ setup.sh
@@ -4,20 +4,20 @@ source /e906/app/software/osg/software/e1039/this-e1039.sh
 source /seaquest/users/dinupa/seaquest/apps/RooUnfold/build/setup.sh
 
 
-function update{
+function update {
     echo "---> Updating the branch"
     git pull
 }
 
 
-function chamber-acceptance{
+function chamber-acceptance {
     echo "---> Calculate the chamber acceptance"
     root -b -q ChamberAcceptance.cc
     echo "---> Plots are saved in imgs"
 }
 
 
-function detector-smearing{
+function detector-smearing {
     echo "---> Calculate the detector smearing using RooUnfold"
     root -b -q DetectorSmearing.cc
 }
